63d25b4da6635b2d804ec4b41f88f617a7ccd9f3
danisaza1 <dani_isaza@htomail.com>
2026-01-26 15:29:39 +0100

    changes front

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,18 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  allowedDevOrigins: [
+    "http://localhost:3000", // Desarrollo local
+    "http://localhost:3001", // Backend local
+    process.env.FRONTEND_URL, // Tu URL de Vercel (variable de entorno)
+    "https://microblogging-three.vercel.app", // Reemplaza con tu dominio real
+    /https:\/\/.*\.vercel\.app$/,
+  ],
   images: {
     domains: [
       "res.cloudinary.com",
-      "images.unsplash.com", 
-      "unsplash.com",         
-      "via.placeholder.com", 
-      "media.istockphoto.com"
-  
-    ]
+      "images.unsplash.com",
+      "unsplash.com",
+      "via.placeholder.com",
+      "media.istockphoto.com",
+    ],
   },
 };
 
 export default nextConfig;
-
-
-
